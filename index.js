9d3aa6d5c3369ee73f24e3efc635ce855a3820bd
Christopher Mena <mena.christopher.student@gmail.com>
2023-03-03 00:31:26 -0500

    Moved files, working simple app

--- index.js
+++ index.js
@@ -1,38 +1,67 @@
-const Papa = require('papaparse');
-const fs = require('fs');
-const path = require('path');
+// Labels
+const Result = {
+  0: "aerosol",
+  1: "battery",
+  2: "cardboard",
+  3: "food can",
+  4: "glass bottle",
+  5: "ink cartridge",
+  6: "magazine",
+  7: "paper bag",
+  8: "plastic bag",
+  9: "plastic bottle",
+  10: "plastic utensils",
+  11: "soda can"
+};
 
-// Read the CSV file
-fs.readFile('meta_df.csv', 'utf8', (err, csvData) => {
-  if (err) throw err;
+  // Load the model and get elemts from the DOM
+const imageSelector = document.getElementById("image-selector");
+const selectedImage = document.getElementById("selected-image");
+const model = await tf.loadGraphModel("./model/model.json");
 
-  // Parse the CSV data
-  const parsedData = Papa.parse(csvData, {
-    header: true,
-    skipEmptyLines: true
-  }).data;
-
-  // Create the DATASET directory if it doesn't exist
-  const datasetDir = './DATASET';
-  if (!fs.existsSync(datasetDir)) {
-    fs.mkdirSync(datasetDir);
-  }
-
-  // Loop through the parsed data and organize the images by category
-  parsedData.forEach(row => {
-    const catName = row.cat_name;
-    const imgFile = row.img_file;
-    const imgSlide = imgFile.slice(8);
-    const imgPath = path.join('data', imgFile);
-    const catDir = path.join(datasetDir, catName);
-    // Create the category directory if it doesn't exist
-    if (!fs.existsSync(catDir)) {
-      fs.mkdirSync(catDir);
+// Get the image from the user
+imageSelector.addEventListener("change", getUploadImage);
+function getUploadImage() {
+  let reader = new FileReader();
+  reader.onload = function() {
+    let dataURL = reader.result;
+    document.getElementById("selected-image").setAttribute("src", dataURL);
+    let predictionList = document.getElementById("prediction-list");
+    // If the list is not empty, clear it
+    if (predictionList) {
+      predictionList.innerHTML = "";
     }
-    // Copy the image to the category directory
-    fs.copyFile(imgPath, path.join('DATASET', catName, imgSlide), err => {
-      if (err) throw err;
-    });
+  };
+  // Read the image
+  let file = document.getElementById("image-selector").files[0];
+  reader.readAsDataURL(file);
+}
+
+  // Predict the image
+const predictButton = document.getElementById("predictBtn");
+const resultList = document.getElementById("list");
+// When the predict button is clicked, run the predict function
+predictButton.addEventListener("click", async function () {
+  const image = selectedImage;
+  const pre_image = tf.browser.fromPixels(image, 3)
+    .resizeNearestNeighbor([224, 224])
+    .expandDims()
+    .toFloat()
+    .reverse(-1);
+  const predict_result = await model.predict(pre_image).data();
+  console.log(predict_result);
+  const order = Array.from(predict_result)
+    .map(function (p, i) {
+      return {
+        probability: p,
+        className: Result[i]
+      };
+    }).sort(function (a, b) {
+      return b.probability - a.probability;
+    }).slice(0, 2);
+    // Display the result
+  resultList.innerHTML = "";
+  order.forEach(function (p) {
+    resultList.insertAdjacentHTML("beforeend", `<li>${p.className}: ${parseInt(Math.trunc(p.probability * 100))} %</li>`);
   });
-  console.log('All images copied successfully')
 });
